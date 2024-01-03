2e50470b9ca72d58832685744ee4982882258d74
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-01-03 12:14:29 +0200

    fix

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -3,13 +3,13 @@ set -o allexport; source .env; set +o allexport;
 
 if [[ $lshw_output == *nvidia* ]]; then
     echo "Installing the GPU version..."
-    sed -i 's/^#deploy:/deploy:/' ./docker-compose.yml
-    sed -i 's/^#  resources:/  resources:/' ./docker-compose.yml
-    sed -i 's/^#    reservations:/    reservations:/' ./docker-compose.yml
-    sed -i 's/^#      devices:/      devices:/' ./docker-compose.yml
-    sed -i 's/^#        - driver: nvidia/        - driver: nvidia/' ./docker-compose.yml
-    sed -i 's/^#          count: all/          count: all/' ./docker-compose.yml
-    sed -i 's/^#          capabilities: \[gpu\]/          capabilities: \[gpu\]/' ./docker-compose.yml
+    sed -i 's/#deploy:/deploy:/g' ./docker-compose.yml
+    sed -i 's/#  resources:/  resources:/g' ./docker-compose.yml
+    sed -i 's/#    reservations:/    reservations:/g' ./docker-compose.yml
+    sed -i 's/#      devices:/      devices:/g' ./docker-compose.yml
+    sed -i 's/#        - driver: nvidia/        - driver: nvidia/g' ./docker-compose.yml
+    sed -i 's/#          count: all/          count: all/g' ./docker-compose.yml
+    sed -i 's/#          capabilities: \[gpu\]/          capabilities: \[gpu\]/g' ./docker-compose.yml
 else
     echo "Installing the CPU version..."
 fi
