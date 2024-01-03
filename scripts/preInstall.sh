886f4ac694d4039fb391436c37a862c9dff696ba
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-01-03 11:32:19 +0200

    fix

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -1,3 +1,15 @@
 #set env vars
 set -o allexport; source .env; set +o allexport;
 
+if [[ $lshw_output == *nvidia* ]]; then
+    echo "Installing the GPU version..."
+    sed -i 's/^#deploy:/deploy:/' ./docker-compose.yml
+    sed -i 's/^#  resources:/  resources:/' ./docker-compose.yml
+    sed -i 's/^#    reservations:/    reservations:/' ./docker-compose.yml
+    sed -i 's/^#      devices:/      devices:/' ./docker-compose.yml
+    sed -i 's/^#        - driver: nvidia/        - driver: nvidia/' ./docker-compose.yml
+    sed -i 's/^#          count: all/          count: all/' ./docker-compose.yml
+    sed -i 's/^#          capabilities: \[gpu\]/          capabilities: \[gpu\]/' ./docker-compose.yml
+else
+    echo "Installing the CPU version..."
+fi
